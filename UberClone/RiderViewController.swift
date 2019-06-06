588a89d50abca23a16c5dfd68098ee8bfe7ca9d2
Wilmer <wilmersinchi@gmail.com>
2019-06-06 12:57:37 -0400

    Firebase database upadted user location, log out button enable, improve map pin

--- UberClone/RiderViewController.swift
+++ UberClone/RiderViewController.swift
@@ -31,6 +31,23 @@ class RiderViewController: UIViewController, CLLocationManagerDelegate{
         locationManger.requestWhenInUseAuthorization()
         locationManger.startUpdatingLocation()
         // Do any additional setup after loading the view.
+        checkIfUberCalled()
+    }
+    
+    func checkIfUberCalled(){
+        //this func help see if uber has been called when your logged out.. it first check the info in the database, if there is than the title of the button will chnage and the snapref,remove.value wont be called since we did mot implemtied 
+        if let email = Auth.auth().currentUser?.email{
+            Database.database().reference().child("RideRequests").queryOrdered(byChild: "email").queryEqual(toValue: email).observe(.childAdded) { (snapchat) in
+                //if we find any information, we dont want to remove the value
+                //                snapchat.ref.removeValue() // that why we dont nees this code here
+                self.uberHasBeenCalled = true
+                self.callInButton.setTitle("Cancel Uber", for: .normal)
+                Database.database().reference().child("RideRequests").removeAllObservers()
+                
+                
+            }
+            
+        }
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
@@ -44,7 +61,7 @@ class RiderViewController: UIViewController, CLLocationManagerDelegate{
             //so it doesnt give new annotation everytime it is updated
             mapView.removeAnnotations(mapView.annotations)
             
-            //to create a pin in the map
+            //to create a pin in the map, the red, you can also customize it with your desire
             let annotation = MKPointAnnotation()
             annotation.coordinate = center
             annotation.title = "My Location"
@@ -63,6 +80,17 @@ class RiderViewController: UIViewController, CLLocationManagerDelegate{
 //    }
     
     @IBAction func logOutAction(_ sender: Any) {
+        //to create your own custom animation!
+        let transition = CATransition()
+        transition.duration = 0.5
+        transition.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+        transition.type = CATransitionType.push
+        transition.subtype = CATransitionSubtype.fromLeft
+        self.view.window!.layer.add(transition, forKey: nil)
+        //must have a try
+        try? Auth.auth().signOut()
+        //have the animation false otherwise it wont work
+        navigationController?.dismiss(animated: false, completion: nil)
         
         
         
@@ -74,16 +102,33 @@ class RiderViewController: UIViewController, CLLocationManagerDelegate{
         
         if let email = Auth.auth().currentUser?.email {
             
-     
+            if uberHasBeenCalled {
+                uberHasBeenCalled = false
+                //change the button once the button has been tapped, it change the button title
+                callInButton.setTitle("Call in Uber", for: .normal)
+                //to find or accesing the currrent email in firebase database that has RideRequest,
+                Database.database().reference().child("RideRequests").queryOrdered(byChild: "email").queryEqual(toValue: email).observe(.childAdded) { (snapchat) in
+                    /*if you leave it like this it would delte everything under RiderRequest snapchat.ref.removeValue(), but also when you call in uber it will also delete it as well becuase when the childisadded it would delte it right away  since it will always try to listen and remove the value, to fix this we havet oremove the obsever so it can stop right away */
+                    snapchat.ref.removeValue()
+                    
+                    Database.database().reference().child("RideRequests").removeAllObservers()
+                }
+                
+                
+            } else {
+                
+            
+     //update the database with the location ofthe current location,
         
         let rideRequestDictionary : [String:Any] = ["email": email, "lat" :userLocation.latitude, "long":userLocation.longitude]
         Database.database().reference().child("RideRequests").childByAutoId().setValue(rideRequestDictionary)
         
             print("button clicked")
-            
+            //so we know that uber has been called
             uberHasBeenCalled = true
-            
+            //change the button once the button has been tapped, it change the button title
             callInButton.setTitle("Cancel Uber", for: .normal)
+            }
         }
      }
     
